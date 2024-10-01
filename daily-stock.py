13e1ffaf9eacc3c9cb2582b67a4a06516a903bbb
Jusung Hwang <j.hwang-3@student.tudelft.nl>
2024-10-01 12:58:25 +0200

    Update daily-stock.py

--- daily-stock.py
+++ daily-stock.py
@@ -1,1 +1,7 @@
-
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
